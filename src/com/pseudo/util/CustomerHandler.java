d79421e4c1c7c253d379afccda9e2608e51807bd
Jishnu Radhakrishnan <47587580+jishnuradhakrishnan@users.noreply.github.com>
2019-04-18 16:03:29 +0530

    Update CustomerHandler.java

--- src/com/pseudo/util/CustomerHandler.java
+++ src/com/pseudo/util/CustomerHandler.java
@@ -4,26 +4,35 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CustomerHandler {
+	//flag to check whether tellers are busy or not
 	private boolean tellerOneHaveCustomer = false;
 	private boolean tellerTwoHaveCustomer = false;
+	
+	//adding customers into queue
 	private LinkedList<String> tellerOneQueue = new LinkedList<String>();
 	private LinkedList<String> tellerTwoQueue = new LinkedList<String>();
+	
+	//this indicates the number of customers served by both tellers
 	private int tellerOneServedCustomers = 0;
 	private int tellerTwoServedCustomers = 0;
-	// 480 seconds
-	long waitTime = 18000;
+	
+	// in one day bank will only work for 480 seconds
+	long waitTime = 480000;
 
 	public void startTransaction() throws Exception {
 		long startTime = System.currentTimeMillis();
 		Scanner in = new Scanner(System.in);
 
-		// app will end after 480 seconds from start time
+		// bank will close after 480 seconds from startTime
 		long endTime = startTime + waitTime;
 
 		while (System.currentTimeMillis() < endTime) {
 
+			//in this loop each and ever customer arrived is added to queue
 			System.out.println("enter customer name ");
 			String customerName = in.next();
+			
+			//if amy have more than 5 customers in her queue she will pass new customers to ana
 			if (tellerOneQueue.size() < 5) {
 				System.out.println("customer " + customerName + " is on teller one");
 				tellerOneQueue.add(customerName);
@@ -38,8 +47,10 @@ public class CustomerHandler {
 						}
 					}
 				};
+				//all customers is considered as a seperate thread(sorry thats the rule!!)
 				Thread t = new Thread(r1);
 				t.start();
+				//else is for ana
 			} else {
 				System.out.println("customer " + customerName + " is on teller two");
 				tellerTwoQueue.add(customerName);
@@ -59,6 +70,7 @@ public class CustomerHandler {
 			}
 		}
 
+		//after transaction closed it will print the report as below
 		System.out.println("Banking Time is over");
 		System.out.println("teller one served " + tellerOneServedCustomers + " customers");
 		System.out.println("teller two served " + tellerTwoServedCustomers + " customers");
@@ -70,14 +82,20 @@ public class CustomerHandler {
 		// this teller needs 1 second to serve her customer
 		long tellerOneCustomerServiceTime = 1000 * 1;
 		System.out.println("teller one Says: I have " + tellerOneQueue.size() + " customers");
+		
+		//this is an infinite loop, it wil end if the flag set false....
 		while (tellerOneHaveCustomer) {
 		}
 		tellerOneHaveCustomer = true;
 
+		//this is the time for serving her customer
 		Thread.sleep(tellerOneCustomerServiceTime);
+		//after customer left the counter amy will say as follows
 		System.out.println("customer " + customerName + " has left from teller one");
 
+		//she is free now
 		tellerOneHaveCustomer = false;
+		//removing the first customer standing at front of the queue
 		tellerOneQueue.remove(0);
 		tellerOneServedCustomers++;
 
@@ -87,14 +105,20 @@ public class CustomerHandler {
 		// this teller needs 2 second to serve her customer
 		long tellerOneCustomerServiceTime = 2000 * 1;
 		System.out.println("teller two Says: I have " + tellerTwoQueue.size() + " customers");
+		
+		//this is an infinite loop, it wil end if the flag set false....
 		while (tellerTwoHaveCustomer) {
 		}
 		tellerTwoHaveCustomer = true;
 
+		//this is the time for serving her customer
 		Thread.sleep(tellerOneCustomerServiceTime);
-		System.out.println("customer " + customerName + " has left from teller one");
+		//after customer left the counter ana will say as follows
+		System.out.println("customer " + customerName + " has left from teller two");
 
+		//she is free now
 		tellerTwoHaveCustomer = false;
+		//removing the first customer standing at front of the queue
 		tellerTwoQueue.remove(0);
 		tellerTwoServedCustomers++;
 
