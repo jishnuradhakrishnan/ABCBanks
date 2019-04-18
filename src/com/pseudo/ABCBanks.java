e729d79553080a7b03b1b581f28c28c77f0739c0
Jishnu Radhakrishnan <47587580+jishnuradhakrishnan@users.noreply.github.com>
2019-04-18 16:03:50 +0530

    Update ABCBanks.java

--- src/com/pseudo/ABCBanks.java
+++ src/com/pseudo/ABCBanks.java
@@ -8,18 +8,27 @@ import com.pseudo.util.CustomerHandler;
 
 @SpringBootApplication
 public class ABCBanks implements CommandLineRunner {
+	/*this is a spring boot application
+	 * a bank named ABC have 2 tellers amy and ana. 
+	 * 
+	 */
 	public static void main(String[] args) {
+		//strting the spring boot application
 		SpringApplication.run(ABCBanks.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		// lets wait one second
+		// bank needs atleast 1 second time to start their transaction
 		Thread.sleep(1000);
+		//calling customerHandler util class
 		CustomerHandler customerHandler = new CustomerHandler();
+		//starting todays transaction
 		customerHandler.startTransaction();
 
+		//the system exists after 480 seconds, it wont care about how many people are there in the queue
+		System.exit(0);
 	}
 
 }
